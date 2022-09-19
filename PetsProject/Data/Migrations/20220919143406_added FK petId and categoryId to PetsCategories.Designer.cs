0b97681cd7264a4446c535ba69068e1b6a7c0bdd
fabio.carapinha <fabio.carapinha@worldit.pt>
2022-09-19 15:34:50 +0100

    added migration for changes on PetsCategories Model

--- PetsProject/Data/Migrations/20220919143406_added FK petId and categoryId to PetsCategories.Designer.cs
+++ PetsProject/Data/Migrations/20220919143406_added FK petId and categoryId to PetsCategories.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PetsProject.Data;
 
@@ -11,9 +12,10 @@ using PetsProject.Data;
 namespace PetsProject.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220919143406_added FK petId and categoryId to PetsCategories")]
+    partial class addedFKpetIdandcategoryIdtoPetsCategories
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PetsProject/Data/Migrations/20220919143406_added FK petId and categoryId to PetsCategories.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetsProject.Data.Migrations
+{
+    public partial class addedFKpetIdandcategoryIdtoPetsCategories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PetsCategories_Categories_CategoriesId",
+                table: "PetsCategories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PetsCategories_Pets_PetsId",
+                table: "PetsCategories");
+
+            migrationBuilder.RenameColumn(
+                name: "PetsId",
+                table: "PetsCategories",
+                newName: "PetId");
+
+            migrationBuilder.RenameColumn(
+                name: "CategoriesId",
+                table: "PetsCategories",
+                newName: "CategoryId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_PetsCategories_PetsId",
+                table: "PetsCategories",
+                newName: "IX_PetsCategories_PetId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_PetsCategories_CategoriesId",
+                table: "PetsCategories",
+                newName: "IX_PetsCategories_CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PetsCategories_Categories_CategoryId",
+                table: "PetsCategories",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PetsCategories_Pets_PetId",
+                table: "PetsCategories",
+                column: "PetId",
+                principalTable: "Pets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PetsCategories_Categories_CategoryId",
+                table: "PetsCategories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PetsCategories_Pets_PetId",
+                table: "PetsCategories");
+
+            migrationBuilder.RenameColumn(
+                name: "PetId",
+                table: "PetsCategories",
+                newName: "PetsId");
+
+            migrationBuilder.RenameColumn(
+                name: "CategoryId",
+                table: "PetsCategories",
+                newName: "CategoriesId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_PetsCategories_PetId",
+                table: "PetsCategories",
+                newName: "IX_PetsCategories_PetsId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_PetsCategories_CategoryId",
+                table: "PetsCategories",
+                newName: "IX_PetsCategories_CategoriesId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PetsCategories_Categories_CategoriesId",
+                table: "PetsCategories",
+                column: "CategoriesId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PetsCategories_Pets_PetsId",
+                table: "PetsCategories",
+                column: "PetsId",
+                principalTable: "Pets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
